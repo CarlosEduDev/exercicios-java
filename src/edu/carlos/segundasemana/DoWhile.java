aa12e845787317d56eb05f2c4b30502871fa92b2
Carlos Eduardo <carloseduardoevang@gmail.com>
2024-04-29 10:16:58 -0300

    feat: iniciando a segunda semana de estudos

--- src/edu/carlos/segundasemana/DoWhile.java
+++ src/edu/carlos/segundasemana/DoWhile.java
@@ -1,4 +1,23 @@
 package edu.carlos.segundasemana;
 
+import java.util.Random;
+
 public class DoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+
+        do{
+            //executando repetidas vezes até alguém atender
+            System.out.println("Telefone tocando");
+        } while (tocando());
+
+        System.out.println("Alô?");
+    }
+
+    private static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3)== 1;
+        System.out.println("Atendeu? " + atendeu);
+
+        return ! atendeu;
+    }
 }
